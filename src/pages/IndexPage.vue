33d1fa97e15bf895b04cd0698665d57afb276c50
caoshouling <1046757614@qq.com>
2024-05-14 16:33:40 +0800

    第一版代码

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+    <p><button @click="syncSendLog">日志测试</button></p>
+
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+function syncSendLog() {
+  window.logger.info('sdf ');
+}
+</script>
